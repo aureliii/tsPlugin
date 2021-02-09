9624f90d54d8425be2fab02a7a9435806dd57b0e
amrruku <aurel.mrruku@fastweb.it>
2021-02-09 16:19:31 +0100

    ref

--- app/userPermissionFix.js
+++ app/userPermissionFix.js
@@ -1,21 +1,78 @@
-var fs = require('fs');
-// var js2xmlparser = require("js2xmlparser");
-var dirname='../force-app/main/default/profiles/';
-// var format = require('xml-formatter');
-let objects = [];
-var jsonTemplate;
 
 
+async function fix(orgMeta, userPermissionStart){
+	var userPermissionNames = userPermissionStart;
+	try {
+    	
+        for(const k of orgMeta.keys()){ 
+			let metadataProfile = orgMeta.get(k);
+			const objectNames = [];
+			
+			if(metadataProfile.hasOwnProperty('userPermissions') && typeof metadataProfile.userPermissions !== 'undefined'){
+				if(metadataProfile.custom === 'false'){
+					metadataProfile.userPermissions = [];
+					orgMeta.set(k, metadataProfile);
+				} else {
+          			if (!Array.isArray(metadataProfile.userPermissions)) {
+						if (objectNames.includes(metadataProfile.userPermissions.name)) {
+						  	objectNames.push(metadataProfile.userPermissions.name);
+						}             
+					  	metadataProfile.userPermissions = Object.entries(metadataProfile.userPermissions);
+				  	} else {
+						//console.log('metadataProfile.userPermission.lenght: '+metadataProfile.userPermission.length);
+						for(var userPermission of metadataProfile.userPermissions){
+						  	if ( userPermission  !== null && userPermission.name !== null  && !objectNames.includes(userPermission.name)) {
+								//console.log('User Permission Name: '+ userPermission.name);
+                				objectNames.push(userPermission.name);
+						  	}
+						}
+					}
 
-async function fix(orgMeta){
+					console.log('objectNames: ', objectNames);
+          
+					//console.log('User Permission Name First Elem: '+ userPermissionNames[0]);
+					let difference = userPermissionNames.filter(x => !objectNames.includes(x));
+
+					//console.log('difference.length: '+difference.length);
+
+					console.log('objectNames: '+ metadataProfile.fullName);
+					console.log('DIFFERENCE: '+JSON.stringify(difference));
+              
+              		Object.entries(difference).forEach(([key, value]) => {
+                		var newObjPerm = {
+							enabled: 'false',
+							name: value
+						};
+						//console.log('newObjPerm:'+ JSON.stringify(newObjPerm));
+                		metadataProfile.userPermissions.push(newObjPerm);               
+              		});
+
+              		metadataProfile.userPermissions = metadataProfile.userPermissions.sort((a, b) => (a.name > b.name) ? 1 : -1);
+				}
+
+				orgMeta.set(k, metadataProfile);
+			}
+                      
+        };
+        
+   		return orgMeta;
+	} catch (error) {
+    	console.log('sono nel catch di f() userPermission!' + error.message);
+    }
+}
+
+async function fix2(orgMeta){
     // console.log('profile struct   ',orgMeta);
     for(const k of orgMeta.keys()){
-    //    console.log(' profile name nel ciclo  '+k);
+        // console.log(' profile name nel ciclo  '+k);
         let profile = orgMeta.get(k);
         if(profile.custom === 'false' && profile.hasOwnProperty('userPermissions') && typeof profile.userPermissions !== 'undefined'){
             profile.userPermissions = [];
             orgMeta.set(k, profile);
         }
+
+
+
         //  objectNames.push(permObject.object);
 /*
         profile["@"]={"xmlns":"http://soap.sforce.com/2006/04/metadata"};
